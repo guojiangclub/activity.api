b9ff514de7d1ee64c4a67401cf75f2399f7804d2
lifeagin <yaochao133@163.com>
2019-12-02 17:56:49 +0800

    修复活动支付接口

--- modules/Component/Payment/src/Services/PayService.php
+++ modules/Component/Payment/src/Services/PayService.php
@@ -2,35 +2,24 @@
 
 namespace GuoJiangClub\Component\Payment\Services;
 
-use Carbon\Carbon;
-use GuoJiangClub\Component\Balance\Model\Balance;
-use GuoJiangClub\Component\Balance\Model\BalanceOrder;
-use GuoJiangClub\Component\User\Models\User;
-use GuoJiangClub\Component\User\Models\UserBind;
 use GuoJiangClub\Component\Payment\Models\Payment;
-use GuoJiangClub\Notifications\ChargeSuccess;
 use GuoJiangClub\Activity\Core\Repository\MemberRepository;
 use GuoJiangClub\Activity\Core\Models\PaymentDetail;
-use GuoJiangClub\Activity\Core\Models\Member;
 use GuoJiangClub\Activity\Core\Models\Activity;
 use GuoJiangClub\Activity\Core\Repository\PaymentRepository;
 use iBrand\Component\Point\Repository\PointRepository;
-use Yansongda\Pay\Pay;
-use Yansongda\Pay\Log;
+use iBrand\Component\User\Models\User;
 use Route;
 
 
 class PayService
 {
-    private $orderRepository;
-    private $orderProcessor;
-
     private $member;
     private $paymentRepository;
     private $pointRepository;
 
     public function __construct(
-        , MemberRepository $memberRepository
+        MemberRepository $memberRepository
         , PaymentRepository $paymentRepository
         , PointRepository $pointRepository
     )
@@ -44,78 +33,11 @@ class PayService
     {
         $type = $charge['type'];
         //充值
-        if ($type == 'recharge') {
-            $this->RechargePaySuccess($charge);
-            //订单
-        } else if ($type == 'order') {
-            $this->OrderPaySuccess($charge);
-            //活动
-        } else if ($type == 'activity') {
+        if ($type == 'activity') {
             $this->ActivityPaySuccess($charge);
         }
-
     }
 
-    protected function RechargePaySuccess($charge)
-    {
-
-        $order_no = $charge['out_trade_no'];
-
-        $type = $charge['type'];
-
-        $order = BalanceOrder::where('order_no', $order_no)->first();
-
-        if ($order AND $order->pay_status == 0 AND $order->pay_amount == $charge['total_amount'] * 100) {
-
-            $order->pay_status = 1;
-            $order->pay_time = Carbon::now();
-            $order->save();
-
-            $balance = Balance::create(['user_id' => $order->user_id, 'type' => 'recharge', 'note' => '充值', 'value' => $order->amount, 'origin_id' => $order->id, 'origin_type' => BalanceOrder::class]);
-
-            event('recharge.success', [$order]);
-            $user = User::find($order->user_id);
-            $user->notify(new ChargeSuccess(['charge' => ['user_id' => $order->user_id, 'type' => 'recharge', 'note' => '充值', 'value' => $order->amount, 'origin_id' => $order->id, 'origin_type' => BalanceOrder::class]]));
-        }
-    }
-
-    protected function OrderPaySuccess($charge)
-    {
-        \Log::info($charge);
-        $order_no = $charge['out_trade_no'];
-        $type = $charge['type'];
-        //更改订单状态
-        $order = $this->orderRepository->getOrderByNo($order_no);
-
-        $need_pay = $order->getNeedPayAmount();
-        $pay_state = $charge['total_amount'] * 100 - $need_pay;
-
-        $order_pay = Payment::where('channel_no', $charge['trade_no'])->where('order_id', $order->id)->first();
-        if ($order_pay And $order_pay->channel != 'balance') {
-            return;
-        }
-
-        if ($pay_state >= 0) {
-            $order = $this->orderRepository->getOrderByNo($order_no);
-
-            $payment = new Payment([
-                'order_id' => $order->id,
-                'channel' => $charge['channel'],
-                'amount' => $charge['total_amount'] * 100,
-                'status' => Payment::STATUS_COMPLETED
-                , 'channel_no' => $charge['trade_no'],
-                'pingxx_no' => ''
-                , 'paid_at' => $charge['send_pay_date']
-            ]);
-
-            $order->payments()->save($payment);
-
-            event('order.customer.paid', [$order]);
-
-            $this->orderProcessor->process($order);
-
-        }
-    }
 
     protected function ActivityPaySuccess($charge)
     {
@@ -154,15 +76,6 @@ class PayService
                     'item_id' => $payment->id,
                 ]);
                 event('point.change', $user->id);
-                $user->notify(new PointRecord(['point' => [
-                    'user_id' => $user->id,
-                    'action' => 'activity',
-                    'note' => '活动报名',
-                    'value' => $payment->point,
-                    'valid_time' => 0,
-                    'item_type' => Payment::class,
-                    'item_id' => $payment->id,
-                ]]));
             }
 
             if ($payment->type == 1 || $payment->type == 2) {
